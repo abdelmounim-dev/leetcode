7bfe1e22b0061e4e6176fa829d7bf14d4532a9ba
Abdelmounim Baouia <abdelmounimbaouia@gmail.com>
2024-05-20 01:41:35 +0100

    linked-list-cycle.go, hashTable method

---
+++ linked-list-cycle.go
@@ -0,0 +1,23 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+
+	pointerMap := make(map[*ListNode]int)
+
+	for node := head; node != nil; node = node.Next {
+		if _, ok := pointerMap[node]; ok {
+			return true
+		}
+		pointerMap[node] = node.Val
+	}
+
+	return false
+}
